f4965d676c6ed171ef4f3608b6b5b05b68c956ee
ikatahira <isaque.katahira@fatec.sp.gov.br>
2024-03-11 17:54:50 +0000

    django com bd

---
+++ pastamodel/appmodelo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+class Comentario(models.Model):
+
+
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    texto = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+    hora = models.TimeField(auto_now_add=True)
